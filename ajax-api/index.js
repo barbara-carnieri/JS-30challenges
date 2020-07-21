eaa55cb6fad27cb14dc5c3414a3ffe91d04d54e2
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-07-21 15:54:40 +0200

    ajax final

--- ajax-api/index.js
+++ ajax-api/index.js
@@ -11,6 +11,38 @@ function findMatches(wordToMatch, cities) {
   return cities.filter(place => {
 
     const regex = new RegExp(wordToMatch, 'gi');
-    return place.city.match(regex)
+    return place.city.match(regex) || place.state.match(regex);
   })
 }
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+function displayMatches() {
+  // console.log(this.value);
+  const matchArray = findMatches(this.value, cities);
+  // console.log(matchArray);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    const growth = place.growth_from_2000_to_2013.replace(regex, `<span class="hl">${this.value}</span>`);
+
+    return `
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+      <span class="population">${numberWithCommas(place.population)}</span>
+      <span class="growth">${growth}</span>
+    </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+  
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
