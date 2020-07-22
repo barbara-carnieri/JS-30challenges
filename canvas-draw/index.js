6de21451856312ed3d0d336fa60cfdafd4df133b
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-07-22 15:14:02 +0200

    canvas draw final

--- canvas-draw/index.js
+++ canvas-draw/index.js
@@ -7,17 +7,34 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 30;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(event) {
   if(!isDrawing) return; // stop the fn from running when they are not moused
   console.log(event);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(lastX, lastY);
+  //go to
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+  // [lastX, lastY] = [event.offsetX, event.offsetY];
+  hue++;
 };
 
+canvas.addEventListener('mousedown', (event) => {
+  isDrawing = true;
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+});
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
